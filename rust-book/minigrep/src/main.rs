ceb032c879f8a6ddd0358e54ce09a995ef2226af
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2022-03-15 20:10:03 +0100

    Cambiada la impresión de errores a standard error

--- rust-book/minigrep/src/main.rs
+++ rust-book/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Configuracion::new(&args).unwrap_or_else(|err| {
-        println!("Problema al parsear los argumentos: {}", err);
+        eprintln!("Problema al parsear los argumentos: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Error de minigrep: {}", e);
+        eprintln!("Error de minigrep: {}", e);
 
         process::exit(1);
     }
